cbb00f9774699f77771219c1616f8667208807ff
David-Yuba <dado2012yuba@gmail.com>
2025-12-28 14:31:59 +0100

    Switched to lazy menager and added the same plugins

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,25 @@
+return {
+	{
+		'nvim-telescope/telescope.nvim', tag = 'v0.2.0',
+		dependencies = { 'nvim-lua/plenary.nvim' },
+		
+		keys = {
+			{ '<leader>ff', function() require("telescope.builtin").find_files() end, desc = 'Telescope find files' },
+			{ '<leader>fg', function() require("telescope.builtin").live_grep() end, desc = 'Telescope live grep' },
+			{ '<leader>fu', function() require("telescope.builtin").buffers() end, desc = 'Telescope buffers' },
+
+			{ "<leader>fh", function() require("telescope.builtin").help_tags() end, desc = 'Telescope help tags' },
+			{ '<leader>fb', function()
+				require("telescope").extensions.file_browser.file_browser({
+						path = "%:p:h",
+						select_buffer = true,
+					})
+				end, desc = 'Telescope browse files' },
+
+		},
+	},
+	{
+		'nvim-telescope/telescope-file-browser.nvim',
+		dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' }
+	}
+}
--- vim-plug/pluggins.vim
+++
@@ -1,7 +0,0 @@
-call plug#begin('~/.config/nvim/autoload/pluggins')
-	Plug ('nvim-telescope/telescope.nvim')
-	Plug ('nvim-lua/plenary.nvim')
-	Plug ('nvim-telescope/telescope-file-browser.nvim')
-
-
-call plug#end()
